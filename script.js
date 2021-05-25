75c1665fe7fbeecf3de5fd4c25aa236de4f5c26c
Dominic Long <dominic.long.au@gmail.com>
2021-05-26 03:25:42 +1000

    Add win condition

--- script.js
+++ script.js
@@ -10,7 +10,46 @@ const gameBoard = (() => {
 
     const getGrid = () => grid;
 
-    return {getGrid, play, grid}
+    const checkThree = (str) => {
+        return (str === 'XXX' || str === 'OOO');
+    };
+
+    const winState = () => {
+        // 3 in a row
+        // 012
+        // 345
+        // 678
+
+        // 3 column
+        // 036
+        // 147
+        // 258
+
+        // diagonal
+        // 048
+        // 246
+
+        const winCondition = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+
+        for(let i=0; i<winCondition.length; i++){
+            if(checkThree(winCondition[i].map(i => grid[i]).join(''))) {
+                return true
+            }
+        }
+
+        return false;
+    }
+
+    return {getGrid, play, winState}
 })();
 
 const Player =  (num, mark) => {
@@ -43,5 +82,15 @@ const gameEngine = (() => {
     gameBoard.play('O',5);
     gameBoard.play('X',1);
     gameBoard.play('O',7);
+    gameBoard.play('X',0);
+    gameBoard.play('O',2);
+    gameBoard.play('X',7);
+    gameBoard.play('O',8);
+
+
+    // for(let i=0; i<9; i++){
+    //     gameBoard.getGrid()[i]=i;
+    // }
     displayController.updateBoard(gameBoard);
+    console.log(gameBoard.winState());
 })();
