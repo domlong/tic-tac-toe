1e50fdf3b79f8976b5bc12cd7304977e517bcfe0
Dominic Long <dominic.long.au@gmail.com>
2021-05-26 01:32:00 +1000

    Add displayController

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 const gameBoard = (() => {
-    let grid = [[],[],[]];
+    let grid = new Array(9);
 
-    const play = (symbol, position) => {
-        if(!grid[position]) {
-            grid[position] = symbol;
+    const play = (mark, i) => {
+        if(!grid[i]) {
+            grid[i] = mark;
         }
         return grid;
     };
 
-    return {play}
+    const getGrid = () => grid;
+
+    return {getGrid, play, grid}
 })();
 
-console.log(gameBoard.play('x',[2,1]));
+const Player =  (num, mark) => {
+    this.num = num;
+    this.mark = mark;
+    const getNum = () => num;
+    const getMark = () => mark;
+    return {getNum, getMark}
+}
+
+const displayController = (() => {
+    let squares = document.querySelectorAll('.square');
+
+    const markSquare = (mark, i) => {
+        squares.item(i).textContent = mark;
+    }
+    
+    const updateBoard = (board) => {
+        board.getGrid().forEach(markSquare)
+    }
+
+    return {updateBoard}
+})();
+
+
+const gameEngine = (() => {
+    const player1 = Player(1, 'X');
+    const player2 = Player(2, 'O');
+    gameBoard.play('X',4);
+    gameBoard.play('O',5);
+    gameBoard.play('X',1);
+    gameBoard.play('O',7);
+    displayController.updateBoard(gameBoard);
+})();
